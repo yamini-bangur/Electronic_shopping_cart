718c60f878a44ccc2795e0613cea93e54453e433
yamini-bangur <yamini.bangur@ideavate.com>
2022-01-13 20:34:35 +0530

    changes in package.json

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "plugin:vue/essential",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
