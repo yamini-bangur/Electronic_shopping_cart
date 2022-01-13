9f5065b05cb13f95229240d3649c91e4e418dd91
yamini-bangur <yamini.bangur@ideavate.com>
2022-01-13 20:53:30 +0530

    checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true,
         "node": true
     },
     "extends": "plugin:vue/essential",
     "parserOptions": {
-        "ecmaVersion": 13,
+        "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
