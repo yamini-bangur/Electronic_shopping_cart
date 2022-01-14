55a4813a566390c656f0a56cfca4869a6fb381b0
yamini-bangur <yamini.bangur@ideavate.com>
2022-01-14 16:59:10 +0530

    heroku app issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     "extends": "plugin:vue/essential",
     "parserOptions": {
-        "ecmaVersion": 12,
+        "ecmaVersion": "2021",
         "sourceType": "module"
     },
     "plugins": [
